d9634c2f1f4c2e0ae216b02d7a613f30cfa1d1e7
Richman Larry Clifford <larriche99@gmail.com>
2018-06-22 02:18:12 +0000

    Initial commit

---
+++ downloader.py
@@ -0,0 +1,34 @@
+import os
+import re
+from bs4 import BeautifulSoup
+
+class TreehouseDownloader:
+    def __init__(self, browser, landing_page_url, email, password):
+        """
+        Create a new instance of this class
+        """
+        pass
+
+    def get_step_urls(self):
+        """
+        Get the urls of the steps that have downloadable videos
+        """
+        pass
+
+    def get_video_url(self):
+        """
+        Extract video url from a single tutorial page
+        """
+        pass
+
+    def download_videos(self):
+        """
+        Main method to be called to handle video downloads
+        """
+        pass
+
+    def login(self):
+        """
+        Log in the bot to treehouse
+        """
+        pass
---
+++ readme.md
@@ -0,0 +1,1 @@
+This is a web scraping project to build an automatic course videos downloader that downloads videos from Treehouse. This will spare me the time taken to manually go through video pages and download the videos.
