7e08454afbd01dc0b024ab9a5d94c7999bcb2618
Richman Larry Clifford <larriche99@gmail.com>
2018-06-29 11:59:20 +0000

    Improved command line arguments handling

--- treehouse.py
+++ treehouse.py
@@ -1,21 +1,21 @@
 import os
 import sys
+import argparse
 import mechanize
 import cookielib
 from downloader import TreehouseDownloader
 
 
-def printUsage():
-    print """
-          Usage:
-             python treehouse.py course_url save_folder email password
-         """
+def main():
+      # Set up expected command line arguments
+      parser = argparse.ArgumentParser()
+      parser.add_argument("course_url", help="URL of main course page")
+      parser.add_argument("save_folder", help="subfolder of /downloads to save course videos in")
+      parser.add_argument("email", help="your Treehouse account email")
+      parser.add_argument("password", help="your Treehouse account password")
+      parser.add_argument("-s", "--skip", action="store_true", help="skip already downloaded videos")
 
-inputs = sys.argv
-
-if len(inputs) < 5:
-      printUsage()
-else:
+      # Set up browser
       browser = mechanize.Browser()
       cookie_jar = cookielib.LWPCookieJar()
 
@@ -25,16 +25,16 @@ else:
       browser.addheaders = [('User-agent', user_agent)]
       browser.set_handle_robots(False)
 
-      url, folder, email, password = inputs[1:5]
+      # Extract command line arguments
+      args = parser.parse_args()
 
-      if (len(inputs) == 6) and (inputs[5] == 'true' or inputs[5] == 'false'):
-            if inputs[5] == 'true':
-                  skip = True
-            else:
-                  skip = False
-      else:
-            skip = True
+      url = args.course_url
+      folder = args.save_folder
+      email = args.email
+      password = args.password
+      skip = args.skip
 
+      # Run bot
       downloader = TreehouseDownloader(browser, url, folder, email, password, skip_downloaded=skip)
 
       try:
@@ -43,3 +43,6 @@ else:
             print '\nBye. See you soon :)'
             sys.exit(0)
 
+
+if __name__ == '__main__':
+    main()
